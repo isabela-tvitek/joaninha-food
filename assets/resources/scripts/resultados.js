56398c2a107046e1681ec82213a19e41adb7bd2e
Isabela Vitek <isat12vitek@gmail.com>
2020-10-27 00:17:35 -0300

    Arrumando erro de chamada da funcao

--- assets/resources/scripts/resultados.js
+++ assets/resources/scripts/resultados.js
@@ -1,43 +1,40 @@
 'use strict';
 
-window.onload = function() {
-    document.addEventListener( function(){
-        window.alert('Aqui estão as receitas encontadas');
-    });
-
-    restoreArrayDados();
-};
-//verifica onload
-
 function restoreArrayDados() {
-    console.log('Restaurando um vetor de objetos do local storage.');
-    // console.log(JSON.parse(localStorage.getItem('dados')) +"aqui");
+  console.log('Restaurando um vetor de objetos do local storage.');
+  // console.log(JSON.parse(localStorage.getItem('dados')) +"aqui");
 
-    let myArrayReceita = JSON.parse(localStorage.getItem('cadrastro'));
-    
-    // console.log(myArrayReceita);
+  let myArrayReceita = JSON.parse(localStorage.getItem('cadrastro'));
 
-    let receitas = document.querySelector('div.receitas');
+  // console.log(myArrayReceita);
 
-    for (let i = 0; i < myArrayReceita.length; i++) {
-        let dados = myArrayReceita[i];
-        /**  console.log("receita: " + dados.nomeReceita, "img: " + dados.img,
+  let receitas = document.querySelector('div.receitas');
+
+  for (let i = 0; i < myArrayReceita.length; i++) {
+    let dados = myArrayReceita[i];
+    /**  console.log("receita: " + dados.nomeReceita, "img: " + dados.img,
          "pessoa: " + dados.pessoa, "email: " + dados.email); */
-        
-        receitas.innerHTML += `<article>
+
+    receitas.innerHTML += `<article>
             <img src="${dados.img}"> </img>
-            <h3>${ dados.nomeReceita}</h3>
+            <h3>${dados.nomeReceita}</h3>
             <p>${dados.pessoa}</p>
             <p>${dados.email}</p>
             <a href="./receitas.html">ver receita</a>
             </article> `;
-    }
+  }
 }
 
-setInterval( function() {
+window.onload = function () {
+  document.addEventListener(function () {
+    window.alert('Aqui estão as receitas encontadas');
+  });
 
-    window.location.reload();
-    window.alert('a pagina foi recarregada');
+  restoreArrayDados();
+};
+//verifica onload
 
+setInterval(function () {
+  window.location.reload();
+  window.alert('a pagina foi recarregada');
 }, 600000);
-
